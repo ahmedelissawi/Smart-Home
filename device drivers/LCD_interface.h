fe5a96c14c1e75bcf4a8c485ff9dd2da91a05f07
kerollosgamal97 <kerollosgamal@gmail.com>
2019-05-14 15:37:14 +0200

    servo header

---
+++ device drivers/LCD_interface.h
@@ -0,0 +1,14 @@
+#ifndef servo_H_
+#define servo_H_
+
+typedef unsigned char unit8;
+
+
+//the servo rotates 30 degres in clockwise direction
+void CW(void);
+
+////the servo rotates 30 degres in counter clockwise direction
+void CCW(void);
+
+
+#endif
